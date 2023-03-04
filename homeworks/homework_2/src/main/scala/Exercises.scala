374b9e7d54fb3a4aef4060df320bde7e658faa0b
angst-storm <serezha.kiprin@gmail.com>
2023-03-04 12:17:31 +0500

    completed task 1

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -15,7 +15,11 @@ object Exercises {
   /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
   на 3 или на 5.*/
   /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-  def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
+  def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
+    (iFrom to iTo)
+      .filter(i => i % 3 == 0 || i % 5 == 0)
+      .sum
+  }
 
 
   /*ЗАДАНИЕ II*/
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -8,13 +8,12 @@ object Test extends TestSuite {
       assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
       assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
     }
+    'test_sumOfDivBy3Or5 - {
+      assert(Exercises.sumOfDivBy3Or5(0, 0) == 0)
+      assert(Exercises.sumOfDivBy3Or5(0, 3) == Seq(3).sum)
+      assert(Exercises.sumOfDivBy3Or5(0, 5) == Seq(3, 5).sum)
+      assert(Exercises.sumOfDivBy3Or5(0, 7) == Seq(3, 5, 6).sum)
+      assert(Exercises.sumOfDivBy3Or5(0, 100) == 2418)
+    }
   }
 }
-
-
-//'test_sumOfDivBy3Or5 - {
-//  assert(Exercises.sumOfDivBy3Or5(0, 0) == 0)
-//  assert(Exercises.sumOfDivBy3Or5(0, 3) == 3)
-//  assert(Exercises.sumOfDivBy3Or5(1, 3) == 3)
-//  assert(Exercises.sumOfDivBy3Or5(1, 5) == 8)
-//}
