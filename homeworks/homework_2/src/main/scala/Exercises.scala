5d94d42efc3794e819d73bc6f811bfa3c75aadba
2dftw <realcos@mail.ru>
2023-03-07 21:00:47 +0500

    task 1

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -16,7 +16,11 @@ object Exercises {
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
+        Range.inclusive(iFrom, iTo)
+          .filter(x => x % 3 == 0 || x % 5 == 0)
+          .sum
+    }
 
 
 
@@ -25,7 +29,29 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
+    def primeFactor(number: Int): Seq[Int] = {
+        var result = Set[Int]()
+        var num = number
+        while (num % 2 == 0 && num != 0){
+            result += 2
+            num = num / 2
+        }
+
+        for { i <- 3 to Math.sqrt(number).toInt
+              if num % i == 0
+              } {
+            result += i
+            while (num % i == 0){
+                num = num / i
+            }
+        }
+
+        if (num > 2) {
+            result += num
+        }
+
+        result.to[collection.immutable.Seq]
+    }
 
 
 
@@ -39,16 +65,20 @@ object Exercises {
     case class Vector2D(x: Double, y: Double)
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
-    def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+    def cosBetween(vec0: Vector2D, vec1: Vector2D): Double =
+        scalar(vec0, vec1) / abs(vec0) / abs(vec1)
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, func: (Vector2D, Vector2D) => Double, rightVec0: Vector2D, rightVec1: Vector2D): Double = {
+        val a = func(leftVec0, leftVec1)
+        val b = func(rightVec0, rightVec1)
+        a + b
+    }
+
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
+
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
+
 
 
 
@@ -71,6 +101,14 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = {
+        def sphereVolume(r: Int): Double = 4 * Math.PI * r * r * r / 3
+
+        ballsArray
+          .mapValues(t => t._2 * sphereVolume(t._1))
+          .toSeq
+          .sortBy(t => t._2)
+          .map(t => t._1)
+    }
 
 }
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -1,3 +1,4 @@
+import Exercises.Vector2D
 import utest._
 
 object Test extends TestSuite{
@@ -7,6 +8,55 @@ object Test extends TestSuite{
             assert(Exercises.divBy3Or7(1, 3) == Seq(3))
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
+        }
+
+        'sumOfDivBy3Or5 - {
+            // zero len
+            assert(Exercises.sumOfDivBy3Or5(0, 0) == 0)
+            assert(Exercises.sumOfDivBy3Or5(3, 3) == 3)
+            // no divs
+            assert(Exercises.sumOfDivBy3Or5(0, 1) == 0)
+            assert(Exercises.sumOfDivBy3Or5(7, 8) == 0)
+            // normal input
+            assert(Exercises.sumOfDivBy3Or5(0, 3) == 3)
+            assert(Exercises.sumOfDivBy3Or5(0, 5) == 8)
+            assert(Exercises.sumOfDivBy3Or5(0, 15) == 60)
+            assert(Exercises.sumOfDivBy3Or5(0, 16) == 60)
+        }
+
+        'primeFactor - {
+            assert(Exercises.primeFactor(0) == Seq())
+            assert(Exercises.primeFactor(1) == Seq())
+            assert(Exercises.primeFactor(2) == Seq(2))
+            assert(Exercises.primeFactor(3) == Seq(3))
+            assert(Exercises.primeFactor(4) == Seq(2))
+            assert(Exercises.primeFactor(6) == Seq(2, 3))
+            assert(Exercises.primeFactor(30) == Seq(2, 3, 5))
+        }
+
+        'sumByFunc - {
+            // scalar
+            assert(Exercises.sumScalars(Vector2D(0, 0), Vector2D(0, 0), Vector2D(0, 0), Vector2D(0, 0)) == 0)
+            assert(Exercises.sumScalars(Vector2D(1, 1), Vector2D(1, 1), Vector2D(0, 0), Vector2D(0, 0)) == 2)
+            assert(Exercises.sumScalars(Vector2D(1, 1), Vector2D(1, 1), Vector2D(1, 1), Vector2D(1, 1)) == 4)
+            assert(Exercises.sumScalars(Vector2D(3, 21), Vector2D(4, 13), Vector2D(2, 3), Vector2D(7, 4)) == 311)
+            // cos
+            assert(Math.abs(Exercises.sumCosines(Vector2D(1, 1), Vector2D(1, 1), Vector2D(1, 1), Vector2D(1, 1)) - 2) < 0.001)
+            assert(Math.abs(Exercises.sumCosines(Vector2D(3, 21), Vector2D(4, 13), Vector2D(2, 3), Vector2D(7, 4)) - 1.882) < 0.001)
+        }
+
+        'sortByHeavyweight - {
+            assert(Exercises.sortByHeavyweight(Map()) == Seq())
+            assert(Exercises.sortByHeavyweight(Map(
+                "Heavy" -> (10, 10),
+                "Light" -> (1, 1),
+                "Medium" -> (10, 1))
+            ) == Seq("Light", "Medium", "Heavy"))
+            assert(Exercises.sortByHeavyweight(Map(
+                "VeryLight" -> (3, 3),
+                "Light" -> (100, 100),
+                "NotLight" -> (999, 999))
+            ) == Seq("VeryLight", "Light", "NotLight"))
         }
     }
 }
