68b856c14e7dcb82fb69183d3dd6efa7438a9f69
dbykov-naumen <dbykov@naumen.ru>
2023-03-10 19:43:56 +0500

    add lec3+hw3

---
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -0,0 +1,13 @@
+package homework_3
+
+import utest._
+
+object Test extends TestSuite{
+
+    val tests = Tests{
+        'test_example - {
+            val trueStr = "правда"
+            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+        }
+    }
+}
