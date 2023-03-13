7193d399dbfc0a1bcf37a4b359aaa3a8c525d7d3
angst-storm <serezha.kiprin@gmail.com>
2023-03-13 21:42:34 +0500

    completed task 1

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -1,13 +1,33 @@
 package homework_3
 
+import homework_3.Exercises._
 import utest._
 
-object Test extends TestSuite{
+object Test extends TestSuite {
 
-    val tests = Tests{
-        'test_example - {
-            val trueStr = "правда"
-            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
-        }
+  val prettyBooleanFormattersTestsValues: List[(Any, String)] = List(
+    (true, "правда"),
+    (false, "ложь"),
+    ("строка", "строка"),
+    (1, "1"),
+    (Some(1), "Some(1)"),
+    (None, "None"),
+    (List("a", "b", "c"), "List(a, b, c)")
+  )
+
+  val tests: Tests = Tests {
+    'prettyBooleanFormatter1_test - {
+      for (test <- prettyBooleanFormattersTestsValues)
+        assert(prettyBooleanFormatter1(test._1) == test._2)
     }
+    'prettyBooleanFormatter2_test - {
+      for (test <- prettyBooleanFormattersTestsValues)
+        assert(prettyBooleanFormatter2(test._1) == test._2)
+    }
+    'prettyBooleanFormatter3_test - {
+      for (test <- prettyBooleanFormattersTestsValues) {
+        assert(prettyBooleanFormatter3(test._1) == test._2)
+      }
+    }
+  }
 }
