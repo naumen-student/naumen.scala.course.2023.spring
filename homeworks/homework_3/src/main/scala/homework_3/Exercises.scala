bc5ae11d1be0fe9cd085b8c4195b5782266164b8
NikitaSamkov <nick.samkov@yandex.ru>
2023-03-15 23:49:03 +0500

    homework 3 complete

--- homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
+++ homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
@@ -12,11 +12,14 @@ object Exercises {
      * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе. 
      * Определение типа необходимо для реализации специальной логики работы с Boolean значениями, которая описана в условии выше.
      */
-    def prettyBooleanFormatter1(x: Any): String = ???
+    def prettyBooleanFormatter1(x: Any): String = if (x.isInstanceOf[Boolean]) if (x.asInstanceOf[Boolean]) "правда" else "ложь" else x.toString
 
-    def prettyBooleanFormatter2(x: Any): String = ???
+    def prettyBooleanFormatter2(x: Any): String = x match {
+        case boolean: Boolean => if (boolean) "правда" else "ложь"
+        case _ => x.toString
+    }
 
-    def prettyBooleanFormatter3(x: Any): String = ???
+    def prettyBooleanFormatter3(x: Any): String = if (x.getClass.getTypeName == "Boolean") if (x.asInstanceOf[Boolean]) "правда" else "ложь" else x.toString
 
 
     /**
@@ -26,11 +29,11 @@ object Exercises {
      * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции. 
      * Обратите внимание на возвращаемые типы.
      */
-    def max1(xs: Seq[Int]): Int = ???
+    def max1(xs: Seq[Int]): Int = if (xs.nonEmpty) xs.max else throw new IllegalArgumentException()
 
-    def max2(xs: Seq[Int]): Seq[Int] = ???
+    def max2(xs: Seq[Int]): Seq[Int] = if (xs.nonEmpty) Seq[Int](xs.max) else Seq.empty[Int]
 
-    def max3(xs: Seq[Int]): Option[Int] = ???
+    def max3(xs: Seq[Int]): Option[Int] = if (xs.nonEmpty) Some(xs.max) else None
 
     /**
      * Задание №3
@@ -41,9 +44,13 @@ object Exercises {
     /**
      * Реализуйте на основе нее 3 варианта суммирования 2х чисел, отличающиеся способом передачи этих 2х чисел в функцию sumIntegers.
      * Как минимум одна из реализаций должна использовать тип данных (класс) написанный вами самостоятельно.
-     */ 
-    def sum1(x: Int, y: Int): Int = sumIntegers(???)
-    def sum2(x: Int, y: Int): Int = sumIntegers(???)
-    def sum3(x: Int, y: Int): Int = sumIntegers(???)
+     */
+    class Point(x: Int, y: Int) extends Iterable[Int]   {
+        override def iterator: Iterator[Int] = Seq[Int](x, y).toIterator
+    }
+
+    def sum1(x: Int, y: Int): Int = sumIntegers(Seq[Int](x, y))
+    def sum2(x: Int, y: Int): Int = sumIntegers(List[Int](x, y))
+    def sum3(x: Int, y: Int): Int = sumIntegers(new Point(x, y))
 
 }
