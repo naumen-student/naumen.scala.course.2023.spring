79a335c9631eb78e16b7167f57e55c3d36479026
Ivan Beshkiltsev <ivanbeshka2002@gmail.com>
2023-03-15 13:08:33 +0500

    homework_3 - Бешкильцев Иван

--- homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
+++ homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
@@ -2,48 +2,98 @@ package homework_3
 
 object Exercises {
 
+  /**
+   * Задание №1
+   * Реализуйте функцию, которая принимает любой тип и преобразует его в строку.
+   * Для всех типов кроме Boolean достаточно воспользоваться стандартной функцией .toString.
+   * Для типа Boolean сделайте особое преобразование: true -> "правда", false -> "ложь".
+   *
+   * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе.
+   * Определение типа необходимо для реализации специальной логики работы с Boolean значениями, которая описана в условии выше.
+   */
 
-    /**
-     * Задание №1
-     * Реализуйте функцию, которая принимает любой тип и преобразует его в строку.
-     * Для всех типов кроме Boolean достаточно воспользоваться стандартной функцией .toString.
-     * Для типа Boolean сделайте особое преобразование: true -> "правда", false -> "ложь".
-     *
-     * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе. 
-     * Определение типа необходимо для реализации специальной логики работы с Boolean значениями, которая описана в условии выше.
-     */
-    def prettyBooleanFormatter1(x: Any): String = ???
+  private val trueString = "правда"
+  private val falseString = "ложь"
 
-    def prettyBooleanFormatter2(x: Any): String = ???
+  def prettyBooleanFormatter1(x: Any): String = {
+    if (x.isInstanceOf[Boolean]) {
+      val xBool = x.asInstanceOf[Boolean]
+      if (xBool) {
+        trueString
+      } else {
+        falseString
+      }
+    } else {
+      x.toString
+    }
+  }
 
-    def prettyBooleanFormatter3(x: Any): String = ???
+  def prettyBooleanFormatter2(x: Any): String = {
+    x match {
+      case true => trueString
+      case false => falseString
+      case _ => x.toString
+    }
+  }
+
+  def prettyBooleanFormatter3(x: Any): String = {
+    if (x.getClass == Boolean.box(true).getClass) {
+      val xBool = x.asInstanceOf[Boolean]
+      if (xBool) {
+        trueString
+      } else {
+        falseString
+      }
+    } else {
+      x.toString
+    }
+  }
 
 
-    /**
-     * Задание №2
-     * Реализуйте функцию нахождения максимального числа в переданной коллекции интов (можно использовать все методы стандартной библиотеки).
-     *
-     * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции. 
-     * Обратите внимание на возвращаемые типы.
-     */
-    def max1(xs: Seq[Int]): Int = ???
+  /**
+   * Задание №2
+   * Реализуйте функцию нахождения максимального числа в переданной коллекции интов (можно использовать все методы стандартной библиотеки).
+   *
+   * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции.
+   * Обратите внимание на возвращаемые типы.
+   */
+  @throws(classOf[UnsupportedOperationException])
+  def max1(xs: Seq[Int]): Int = xs.max
 
-    def max2(xs: Seq[Int]): Seq[Int] = ???
+  def max2(xs: Seq[Int]): Seq[Int] = {
+    if (xs.isEmpty) {
+      Seq.empty
+    } else {
+      Seq(xs.max)
+    }
+  }
 
-    def max3(xs: Seq[Int]): Option[Int] = ???
+  def max3(xs: Seq[Int]): Option[Int] = {
+    if (xs.isEmpty) {
+      Option.empty
+    } else {
+      Option(xs.max)
+    }
+  }
 
-    /**
-     * Задание №3
-     * Допустим дана функция sumIntegers, которая умеет суммировать числа.
-     */
-    def sumIntegers[CollectionType <: Iterable[Int]](xs: CollectionType): Int = xs.sum
+  /**
+   * Задание №3
+   * Допустим дана функция sumIntegers, которая умеет суммировать числа.
+   */
+  def sumIntegers[CollectionType <: Iterable[Int]](xs: CollectionType): Int = xs.sum
 
-    /**
-     * Реализуйте на основе нее 3 варианта суммирования 2х чисел, отличающиеся способом передачи этих 2х чисел в функцию sumIntegers.
-     * Как минимум одна из реализаций должна использовать тип данных (класс) написанный вами самостоятельно.
-     */ 
-    def sum1(x: Int, y: Int): Int = sumIntegers(???)
-    def sum2(x: Int, y: Int): Int = sumIntegers(???)
-    def sum3(x: Int, y: Int): Int = sumIntegers(???)
+  /**
+   * Реализуйте на основе нее 3 варианта суммирования 2х чисел, отличающиеся способом передачи этих 2х чисел в функцию sumIntegers.
+   * Как минимум одна из реализаций должна использовать тип данных (класс) написанный вами самостоятельно.
+   */
+  def sum1(x: Int, y: Int): Int = sumIntegers(Iterable(x, y))
+
+  def sum2(x: Int, y: Int): Int = sumIntegers(Seq(x, y))
+
+  def sum3(x: Int, y: Int): Int = sumIntegers(TwoIntegersIterable(x, y))
+
+  case class TwoIntegersIterable(x: Int, y: Int) extends Iterable[Int] {
+    override def iterator: Iterator[Int] = Iterator.apply(x, y)
+  }
 
 }
--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -1,13 +1,89 @@
 package homework_3
 
 import utest._
+import homework_3.Exercises._
 
 object Test extends TestSuite{
 
     val tests = Tests{
-        'test_example - {
+        'test_prettyBooleanFormatter1 - {
             val trueStr = "правда"
-            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+            assert(prettyBooleanFormatter1(true) == trueStr)
+
+            val falseStr = "ложь"
+            assert(prettyBooleanFormatter1(false) == falseStr)
+
+            val char = "c"
+            assert(prettyBooleanFormatter1('c') == char)
+
+            val double = "4.0123"
+            assert(prettyBooleanFormatter1(4.0123) == double)
+        }
+
+        'test_prettyBooleanFormatter2 - {
+            val trueStr = "правда"
+            assert(prettyBooleanFormatter2(true) == trueStr)
+
+            val falseStr = "ложь"
+            assert(prettyBooleanFormatter2(false) == falseStr)
+
+            val char = "c"
+            assert(prettyBooleanFormatter2('c') == char)
+
+            val double = "4.0123"
+            assert(prettyBooleanFormatter2(4.0123) == double)
+        }
+
+        'test_prettyBooleanFormatter3 - {
+            val trueStr = "правда"
+            assert(prettyBooleanFormatter3(true) == trueStr)
+
+            val falseStr = "ложь"
+            assert(prettyBooleanFormatter3(false) == falseStr)
+
+            val char = "c"
+            assert(prettyBooleanFormatter3('c') == char)
+
+            val double = "4.0123"
+            assert(prettyBooleanFormatter3(4.0123) == double)
+        }
+
+        'test_max1 - {
+            intercept[UnsupportedOperationException]{
+                max1(Seq.empty)
+            }
+            assert(max1(Seq(1, 2, 3)) == 3)
+            assert(max1(Seq(1, 1, 1)) == 1)
+        }
+
+        'test_max2 - {
+            assert(max2(Seq.empty) == Seq.empty)
+            assert(max2(Seq(1, 2, 3)) == Seq(3))
+            assert(max2(Seq(1, 1, 1)) == Seq(1))
+        }
+
+        'test_max3 - {
+            assert(max3(Seq.empty) == Option.empty)
+            assert(max3(Seq(1, 2, 3)) == Option(3))
+            assert(max3(Seq(1, 1, 1)) == Option(1))
+        }
+
+        'test_sum1 - {
+            assert(sum1(1, 2) == 3)
+            assert(sum1(0, 0) == 0)
+            assert(sum1(-100, -100) == -200)
+        }
+
+        'test_sum2 - {
+            assert(sum2(1, 2) == 3)
+            assert(sum2(0, 0) == 0)
+            assert(sum2(-100, -100) == -200)
+        }
+
+        'test_sum3 - {
+            assert(sum3(1, 2) == 3)
+            assert(sum3(0, 0) == 0)
+            assert(sum3(-100, -100) == -200)
         }
     }
 }
