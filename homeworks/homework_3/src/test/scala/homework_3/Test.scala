ca9276058641cf768ca549d61b30946c3ec4c616
Simernope <simeon02908@gmail.com>
2023-03-16 13:49:26 +0500

    2 task completed

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -20,5 +20,17 @@ object Test extends TestSuite{
             assert(Exercises.prettyBooleanFormatter1(false) == "ложь")
             assert(Exercises.prettyBooleanFormatter1(12, true, "Hello") == "(12,true,Hello)")
         }
+        'max1 - {
+            assert(Exercises.max1(Seq(1,89,874,32)) == 874)
+            assert(Exercises.max1(Seq()) == 0)
+        }
+        'max2 - {
+            assert(Exercises.max2(Seq()) == List())
+            assert(Exercises.max2(Seq(1,89,874,32)) == List(874))
+        }
+        'max3 - {
+            assert(Exercises.max3(Seq()) == None)
+            assert(Exercises.max3(Seq(1, 89, 874, 32)) == Option(874))
+        }
     }
 }
