7b81bf58a537532eee264a460c4bb59fe26cdae1
ArtakOvakimyan <ovakimyanartak4@gmail.com>
2023-03-20 11:47:56 +0500

    homework_3

--- homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
+++ homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
@@ -1,36 +1,65 @@
-package homework_3
-
 object Exercises {
-
-
     /**
      * Задание №1
      * Реализуйте функцию, которая принимает любой тип и преобразует его в строку.
      * Для всех типов кроме Boolean достаточно воспользоваться стандартной функцией .toString.
      * Для типа Boolean сделайте особое преобразование: true -> "правда", false -> "ложь".
      *
-     * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе. 
+     * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе.
      * Определение типа необходимо для реализации специальной логики работы с Boolean значениями, которая описана в условии выше.
      */
-    def prettyBooleanFormatter1(x: Any): String = ???
+    def prettyBooleanFormatter1(x: Any): String = {
+        x match {
+            case bool: Boolean => if (bool) "правда"
+            else "ложь"
+            case _ => x.toString
+        }
+    }
 
-    def prettyBooleanFormatter2(x: Any): String = ???
+    def prettyBooleanFormatter2(x: Any): String = {
+        if (x.isInstanceOf[Boolean]) {
+            if (x == true) {
+                return "правда"
+            }
+            return "ложь"
+        }
+        x.toString
+    }
 
-    def prettyBooleanFormatter3(x: Any): String = ???
+    def prettyBooleanFormatter3(x: Any): String = {
+        if (x.getClass == classOf[java.lang.Boolean]) {
+            if (x == true)
+                return "правда"
+            return "ложь"
+        }
+        x.toString
+    }
 
 
     /**
      * Задание №2
      * Реализуйте функцию нахождения максимального числа в переданной коллекции интов (можно использовать все методы стандартной библиотеки).
      *
-     * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции. 
+     * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции.
      * Обратите внимание на возвращаемые типы.
      */
-    def max1(xs: Seq[Int]): Int = ???
+    def max1(xs: Seq[Int]): Int = {
+        if (xs.isEmpty)
+            return -1
+        xs.max
+    }
 
-    def max2(xs: Seq[Int]): Seq[Int] = ???
+    def max2(xs: Seq[Int]): Seq[Int] = {
+        if (xs.isEmpty)
+            return Seq()
+        Seq(xs.max)
+    }
 
-    def max3(xs: Seq[Int]): Option[Int] = ???
+    def max3(xs: Seq[Int]): Option[Int] = {
+        if (xs.isEmpty)
+            return None
+        Some(xs.max)
+    }
 
     /**
      * Задание №3
@@ -41,9 +70,11 @@ object Exercises {
     /**
      * Реализуйте на основе нее 3 варианта суммирования 2х чисел, отличающиеся способом передачи этих 2х чисел в функцию sumIntegers.
      * Как минимум одна из реализаций должна использовать тип данных (класс) написанный вами самостоятельно.
-     */ 
-    def sum1(x: Int, y: Int): Int = sumIntegers(???)
-    def sum2(x: Int, y: Int): Int = sumIntegers(???)
-    def sum3(x: Int, y: Int): Int = sumIntegers(???)
-
+     */
+    case class Pair(x: Int, y: Int) extends Iterable[Int] {
+        override def iterator: Iterator[Int] = Iterator(x, y)
+    }
+    def sum1(x: Int, y: Int): Int = sumIntegers(Seq(x, y))
+    def sum2(x: Int, y: Int): Int = sumIntegers(List(x, y))
+    def sum3(x: Int, y: Int): Int = sumIntegers(Pair(x, y))
 }
