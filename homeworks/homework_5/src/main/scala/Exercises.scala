e6ce95ecc364793591f71d39d6e8baf5fa9c5389
NikitaSamkov <nick.samkov@yandex.ru>
2023-04-07 13:57:45 +0500

    homework complete

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -9,15 +9,47 @@ object Exercises {
 
 
 
-  case class Shelter ...
+  case class Shelter[+T <: Animal](private val animals: Iterable[T]) {
+    val animalsList: List[T] = animals.toList
+
+    def +[TOther >: T <: Animal](newAnimal: TOther): Shelter[TOther] = {
+      Shelter(animalsList :+ newAnimal)
+    }
+
+    def ++[TOther >: T <: Animal](other: Shelter[TOther]): Shelter[TOther] = {
+      Shelter(this.animalsList ++ other.animalsList)
+    }
+
+    def getNames: Iterable[String] = {
+      animalsList.map(e => e.name)
+    }
+
+    def feed[TFood >: T](food: Food[TFood]): Iterable[String] = {
+      animalsList.map(e => food.feed(e))
+    }
+  }
 
 
 
-  trait Food ...
+  trait Food[-T] {
+    def feed(animal: T): String
+  }
 
-  case object Meat extends Food[Animal] ...
+  case object Meat extends Food[Animal] {
+    def feed(animal: Animal): String = {
+      f"${animal.name} eats meat"
+    }
+  }
 
-  case object Milk extends Food[Cat] ...
+  case object Milk extends Food[Cat] {
+    def feed(animal: Cat): String = {
+      f"${animal.name} eats milk"
+    }
+  }
 
-  case object Bread extends Food[Dog] ...
+  case object Bread extends Food[Dog] {
+    def feed(animal: Dog): String = {
+      f"${animal.name} eats bread"
+    }
+  }
 }
