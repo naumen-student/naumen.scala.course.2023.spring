9f35506c3582c1d9fb84312ccea7813bf04552ba
angst-storm <serezha.kiprin@gmail.com>
2023-04-08 22:30:28 +0500

    completed task 2

--- homeworks/homework_5/src/main/scala/Shelter.scala
+++ homeworks/homework_5/src/main/scala/Shelter.scala
@@ -4,4 +4,6 @@ case class Shelter[+A <: Animal](animals: List[A]) {
   def ++[B >: A <: Animal](other: Shelter[B]): Shelter[B] = Shelter(animals ++ other.animals)
 
   def getNames: List[String] = animals.map(_.name)
+
+  def feed(food: Food[A]): List[String] = animals.map(animal => food.feed(animal))
 }
