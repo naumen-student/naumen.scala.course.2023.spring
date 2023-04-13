f08bc4058483e33db10bc50e1036b7619e7b9d72
Simernope <simeon02908@gmail.com>
2023-04-13 14:20:22 +0500

    the second task is completed

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -18,16 +18,25 @@ object Exercises {
 
     def getNames: List[String] = list.map(_.name)
 
-
+    def feed[A >: T <: Animal](food: Food[A]): List[String] = list.map(food.feed)
   }
 
 
+  trait Food[T <: Animal] {
+    val foodName: String
 
-  trait Food ...
+    def feed(animal: T): String = s"${animal.name} eats ${foodName.toLowerCase}"
+  }
 
-  case object Meat extends Food[Animal] ...
+  case object Meat extends Food[Animal] {
+    override val foodName: String = "Meat"
+  }
 
-  case object Milk extends Food[Cat] ...
+  case object Milk extends Food[Cat] {
+    override val foodName: String = "Milk"
+  }
 
-  case object Bread extends Food[Dog] ...
+  case object Bread extends Food[Dog] {
+    override val foodName: String = "Bread"
+  }
 }
