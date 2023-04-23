ccf99b71ae7743aa47c989ad8ba12be9ddcb2510
StepanBarkan <Stepan.Barkan@urfu.me>
2023-04-23 15:07:55 +0500

    the first task is completed

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -8,8 +8,18 @@ object Exercises {
   case class Dog(override val name: String) extends Animal
 
 
+  case class Shelter[+A <: Animal](animals: List[A]) {
+    def +[B >: A <: Animal](animal: B): Shelter[B] = Shelter(animals :+ animal)
 
-  case class Shelter ...
+    def ++[B >: A <: Animal](other: Shelter[B]): Shelter[B] = Shelter(animals ++ other.animals)
+
+    def getNames: List[String] = animals.map(a => a.name)
+
+    def feed(food: Food[A]): List[String] = animals.map(animal => food.feed(animal))
+  }
+
+  trait Food[-T <: Animal] {
+    val name: String = "food
 
 
 
