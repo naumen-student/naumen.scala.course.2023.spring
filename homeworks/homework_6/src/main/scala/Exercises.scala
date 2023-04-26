0b68d144ef7e5dff14482193b6b24ffe440f207b
NikitaSamkov <nick.samkov@yandex.ru>
2023-04-26 19:51:57 +0500

    homework 6 complete

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,7 @@
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.reverse
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +9,19 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = fibonacci(idx).last
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = {
+    if (idx == 0)
+      return Seq(0)
+    if (idx == 1)
+      return Seq(0, 1)
+    val numbers = Array.fill(idx + 1)(0)
+    numbers(1) = 1
+    for (i <- 2 to idx)
+      numbers(i) = numbers(i - 1) + numbers(i - 2)
+    numbers
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +29,35 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  private val CODE = 0
+  private val NONCODE = 1
 
+  def morse(text: String): String = text
+    .toUpperCase()
+    .map(symbol => {
+      val code = MORSE.get(symbol.toString)
+      if (code.isEmpty)
+        (symbol, if (symbol != ' ') NONCODE else CODE)
+      else
+      (code.get, CODE)
+    })
+    .foldLeft(("", NONCODE))((left, right) => if (left._2 == CODE && right._2 == CODE) (left._1 + " " + right. _1, CODE) else (left._1 + right._1, right._2))
+    ._1
 
-  def wordReverse(text: String): String = ???
+  def getNextBlock(str: String): (String, String) = {
+    if (str.isEmpty)
+      return ("", "")
+    val takeRule = if (str(0).isLetter) (c: Char) => c.isLetter else (c: Char) => !c.isLetter
+    val block = str.takeWhile(takeRule)
+    val remaining = str.drop(block.length)
+    (block, remaining)
+  }
+
+  def wordReverse(text: String): String = {
+    val (block, remaining) = getNextBlock(text)
+    if (block.isEmpty)
+      return ""
+    (if (block(0).isLetter) if (block(0).isUpper) (block(0).toLower + block.drop(1)).reverse.capitalize else block.reverse else block) + wordReverse(remaining)
+  }
 
 }
