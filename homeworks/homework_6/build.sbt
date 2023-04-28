f5ba93c59e5f2e38b5dac4871014b6d6de78e94c
lanskikh <lanskikh-mikhail@mail.ru>
2023-04-28 17:19:31 +0500

    task's done

---
+++ homeworks/homework_6/build.sbt
@@ -0,0 +1,12 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.10"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "homework_6"
+  )
+
+libraryDependencies += "com.lihaoyi" %% "utest" % "0.8.1" % "test"
+
+testFrameworks += new TestFramework("utest.runner.Framework")
---
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -0,0 +1,46 @@
+object Exercises {
+
+  def reverse[T](seq: Seq[T]): Seq[T] = seq.reverse
+
+  /**
+   * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
+   *
+   * @param idx
+   * @return
+   */
+  def fibonacci4Index(idx: Int): Int = {
+    require(idx >= 0, "Индекс должен быть не меньше нуля")
+    if (Seq(1, 2).contains(idx)) return 1
+    fibonacci4Index(idx - 1) + fibonacci4Index(idx - 2)
+  }
+
+  def fibonacci(idx: Int): Seq[Int] = {
+    require(idx >= 0, "Индекс должен быть не меньше нуля")
+    if (idx == 1) Seq(0)
+    if (idx == 2) Seq(0, 1)
+    val fib = Array.fill(idx + 1)(0)
+    fib(1) = 1
+    for (i <- 2 to idx) fib(i) = fib(i - 1) + fib(i - 2)
+    fib
+  }
+
+  lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
+    "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+    "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+    "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+    "Y" -> "-.--", "Z" -> "--..")
+
+  def morse(text: String): String = text
+    .map(char => {
+      val code = MORSE.get(char.toUpper.toString)
+      if (code.isEmpty) if (char == '!') char else " " + char else " " + code.get
+    }).mkString("").trim
+
+  def wordReverse(text: String): String = text
+    .split(" ")
+    .map(word => {
+      val (letters, notLetters) = word.partition(_.isLetter)
+      val reversed = letters.reverse.toLowerCase
+      (if (word(0).isUpper) reversed.capitalize else reversed) + notLetters
+    }).mkString(" ")
+}
