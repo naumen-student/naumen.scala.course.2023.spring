e95f52add167b31966d34913b82d20ee6308480f
Hassan <86029009+kyan9400@users.noreply.github.com>
2023-04-29 21:35:04 +0300

    Update Exercises.scala

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -1,23 +1,36 @@
 object Exercises {
   trait Animal {
-    def name: String
+    protected def name: String
   }
 
-  case class Cat(override val name: String) extends Animal
+  case class Cat(name: String) extends Animal
 
-  case class Dog(override val name: String) extends Animal
+  case class Dog(name: String) extends Animal
 
+  case class Shelter[+A <: Animal](animals: List[A]) {
+    def +[B >: A <: Animal](animal: B): Shelter[B] = Shelter(animal :: animals)
 
+    def ++[B >: A <: Animal](other: Shelter[B]): Shelter[B] = Shelter(animals ::: other.animals)
 
-  case class Shelter ...
+    def getNames: List[String] = animals.map(_.name)
 
+    def feed(food: Food[A]): List[String] = animals.map(animal => food.feed(animal))
+  }
 
+  trait Food[-T <: Animal] {
+    val name: String
+    def feed(animal: T): String = s"${animal.name} eats $name"
+  }
 
-  trait Food ...
+  case object Meat extends Food[Animal] {
+    val name: String = "meat"
+  }
 
-  case object Meat extends Food[Animal] ...
+  case object Milk extends Food[Cat] {
+    val name: String = "milk"
+  }
 
-  case object Milk extends Food[Cat] ...
-
-  case object Bread extends Food[Dog] ...
+  case object Bread extends Food[Dog] {
+    val name: String = "bread"
+  }
 }
