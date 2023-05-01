554540d3d53d5ceccc01a7cb7772121a5160c295
Hassan <86029009+kyan9400@users.noreply.github.com>
2023-05-01 03:02:19 +0300

    Update Exercises.scala

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,27 +1,38 @@
-object Exercises {
+import utest._
 
+object Test extends TestSuite {
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  val tests = Tests {
+    'reverse - {
+      assert(Exercises.reverse(Seq(1, 2, 3)) == Seq(3, 2, 1))
+      assert(Exercises.reverse(Seq(-1, -2, -3)) == Seq(-3, -2, -1))
+    }
 
-  /**
-   * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
-   *
-   * @param idx
-   * @return
-   */
-  def fibonacci4Index(idx: Int): Int = ???
+    'fibonacci4Index - {
+      assert(Exercises.fibonacci4Index(2) == 1)
+      assert(Exercises.fibonacci4Index(5) == 5)
+      assert(Exercises.fibonacci4Index(6) == 8)
+      assert(Exercises.fibonacci4Index(7) == 13)
+      assert(Exercises.fibonacci4Index(8) == 21)
+      assert(Exercises.fibonacci4Index(9) == 34)
+      assert(Exercises.fibonacci4Index(10) == 55)
+      assert(Exercises.fibonacci4Index(11) == 89)
+      assert(Exercises.fibonacci4Index(22) == 17711)
+    }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+    'fibonacci - {
+      assert(Exercises.fibonacci(2) == Seq(0, 1, 1))
+      assert(Exercises.fibonacci(5) == Seq(0, 1, 1, 2, 3, 5))
+      assert(Exercises.fibonacci(22) == Seq(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711))
+    }
 
-  lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+    'morse - {
+      assert(Exercises.morse("SOS") == "... --- ...")
+      assert(Exercises.morse("Hello world!") == ".... . .-.. .-.. ---   .-- --- .-. .-.. -..!")
+    }
 
-  def morse(text: String): String = ???
-
-
-  def wordReverse(text: String): String = ???
-
+    'wordReverse - {
+      assert(Exercises.wordReverse("Зима!.. Крестьянин, торжествуя...") == "Амиз!.. Ниняьтсерк, яувтсежрот...")
+    }
+  }
 }
