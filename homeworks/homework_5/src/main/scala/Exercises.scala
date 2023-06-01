6ba0163cc781d452905338971fcbfbd3f32e38bb
Ildar Sabrikhanov <gogifad@gmail.com>
2023-06-01 18:07:37 +0500

    1st task completed

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -9,15 +9,24 @@ object Exercises {
 
 
 
-  case class Shelter ...
+  case class Shelter[+T <: Animal](list: List[T]) {
+    def +[A >: T <: Animal](animal: A): Shelter[A] = Shelter(list :+ animal)
+
+    def ++[A >: T <: Animal](shelter: Shelter[A]): Shelter[A] = {
+      val listA: List[A] = list
+      Shelter(listA ++ shelter.list)
+    }
+
+    def getNames: List[String] = list.map(_.name)
+  }
 
 
 
-  trait Food ...
-
-  case object Meat extends Food[Animal] ...
-
-  case object Milk extends Food[Cat] ...
-
-  case object Bread extends Food[Dog] ...
+//  trait Food ...
+//
+//  case object Meat extends Food[Animal] ...
+//
+//  case object Milk extends Food[Cat] ...
+//
+//  case object Bread extends Food[Dog] ...
 }
