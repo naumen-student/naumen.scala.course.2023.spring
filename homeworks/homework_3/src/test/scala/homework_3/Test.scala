4f612724adad0df313097ea963fe9675fdefbacf
MarkPoloz <147072230+MarkPoloz@users.noreply.github.com>
2023-11-22 20:11:48 +0500

    Update Test.scala

--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -5,9 +5,33 @@ import utest._
 object Test extends TestSuite{
 
     val tests = Tests{
-        'test_example - {
-            val trueStr = "правда"
-            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+        'test1 - {
+            assert(Exercises.prettyBooleanFormatter1(true)=="правда")
+            assert(Exercises.prettyBooleanFormatter1(false)=="ложь")
+            assert(Exercises.prettyBooleanFormatter1(1)=="1")
+
+            assert(Exercises.prettyBooleanFormatter2(true)=="правда")
+            assert(Exercises.prettyBooleanFormatter2(false)=="ложь")
+            assert(Exercises.prettyBooleanFormatter2(2)=="2")
+
+            assert(Exercises.prettyBooleanFormatter3(true)=="правда")
+            assert(Exercises.prettyBooleanFormatter3(false)=="ложь")
+            assert(Exercises.prettyBooleanFormatter3(3)=="3")
+        }
+        'test2 -{
+            intercept[NullPointerException] { Exercises.max1(Seq()) }
+            assert(Exercises.max1(Seq(4,7,2,3))==7)
+
+            assert(Exercises.max2(Seq())==Seq())
+            assert(Exercises.max2(Seq(4,7,2,3))==Seq(7))
+
+            assert(Exercises.max3(Seq()).isEmpty)
+            assert(Exercises.max2(Seq(4,7,2,3)).contains(7))
+        }
+        'test3 - {
+            assert(Exercises.sum1(2,3)==5)
+            assert(Exercises.sum2(2,3)==5)
+            assert(Exercises.sum3(2,3)==5)
         }
     }
 }
